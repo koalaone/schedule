b8044e25f98d5572cb9190aac0dc8db0c8922114
koalaone <koalaone@163.com>
2019-02-08 13:08:28 +0800

    full commit

---
+++ schedule.go
@@ -0,0 +1,249 @@
+/*
+ *
+ *
+ *  * Copyright 2019 koalaone@163.com
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *       http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package schedule
+
+import (
+	"context"
+	"errors"
+	"reflect"
+	"runtime"
+	"sync"
+	"time"
+)
+
+// Time location, default set by the time.Local (*time.Location)
+var timeLocal = time.Local
+
+// Change the time location
+func ChangeTimeLocation(newLocal *time.Location) {
+	timeLocal = newLocal
+}
+
+type Task struct {
+	isOnce   bool
+	interval time.Duration
+	running  bool
+	lastRun  time.Time
+	gName    string
+	gFunc    map[string]interface{}
+	gParams  map[string]([]interface{})
+}
+
+type Scheduler struct {
+	running bool
+	time    *time.Ticker
+	tasks   []*Task
+	sync.RWMutex
+}
+
+var schedule *Scheduler
+
+func newScheduler() *Scheduler {
+	if schedule == nil {
+		schedule = &Scheduler{
+			running: false,
+			tasks:   make([]*Task, 0),
+		}
+	}
+
+	return schedule
+}
+
+func every(interval uint64, once bool) *Task {
+	if interval <= 0 {
+		interval = 1
+	}
+	newTask := &Task{
+		isOnce:   once,
+		interval: time.Duration(interval),
+		lastRun:  time.Now(),
+		gName:    "",
+		gFunc:    make(map[string]interface{}, 0),
+		gParams:  make(map[string]([]interface{}), 0),
+	}
+
+	if once {
+		newTask.lastRun = time.Unix(int64(interval), 0)
+	}
+
+	if schedule == nil {
+		newScheduler()
+	}
+
+	schedule.Add(newTask)
+
+	return newTask
+}
+
+func EverySeconds(interval uint64) *Task {
+	return every(interval, false)
+}
+
+func EveryMinutes(interval uint64) *Task {
+	return every(interval*60, false)
+}
+
+func EveryHours(interval uint64) *Task {
+	return every(interval*60*60, false)
+}
+
+func EveryDays(interval uint64) *Task {
+	return every(interval*60*60*24, false)
+}
+
+func AtDateTime(year int, month time.Month, day, hour, minute, second int) *Task {
+	return every(uint64(time.Date(year, month, day, hour, minute, second, 0, timeLocal).Unix()), true)
+}
+
+func (this *Task) Do(taskFun interface{}, params ...interface{}) error {
+	typ := reflect.TypeOf(taskFun)
+	if typ.Kind() != reflect.Func {
+		return errors.New("param taskFun type error")
+	}
+
+	funcName := runtime.FuncForPC(reflect.ValueOf(taskFun).Pointer()).Name()
+	this.gName = funcName
+	this.gFunc[funcName] = taskFun
+	this.gParams[funcName] = params
+
+	return nil
+}
+
+func (this *Scheduler) checkTaskStatus(isWait bool) bool {
+retry:
+	for _, taskItem := range this.tasks {
+		locTask := taskItem
+
+		if locTask.running {
+			if isWait {
+				time.Sleep(5 * time.Millisecond)
+				goto retry
+			}
+
+			return false
+		}
+	}
+
+	return true
+}
+
+func (this *Task) run(locNow time.Time) (result []reflect.Value, err error) {
+	if this.isOnce && (locNow.Unix()-this.lastRun.Unix() > 0) {
+		return
+	}
+
+	if this.running {
+		return
+	}
+
+	this.running = true
+	defer func() {
+		this.running = false
+	}()
+
+	if !this.isOnce {
+		nextTime := this.lastRun.Add(this.interval * time.Second)
+		if (locNow.Unix() - nextTime.Unix()) < 0 {
+			return
+		}
+	} else {
+		if (locNow.Unix() - this.lastRun.Unix()) < 0 {
+			return
+		}
+	}
+
+	f := reflect.ValueOf(this.gFunc[this.gName])
+	params := this.gParams[this.gName]
+	if len(params) != f.Type().NumIn() {
+		err = errors.New(" param num not adapted ")
+		return
+	}
+	in := make([]reflect.Value, len(params))
+	for k, param := range params {
+		in[k] = reflect.ValueOf(param)
+	}
+	result = f.Call(in)
+
+	if this.isOnce {
+		this.lastRun = time.Unix(0, 0)
+	}
+
+	this.lastRun = locNow
+
+	return
+}
+
+func (this *Scheduler) Add(value *Task) *Scheduler {
+	if this.running {
+		return this
+	}
+
+	if value == nil {
+		return this
+	}
+
+	this.Lock()
+	this.tasks = append(this.tasks, value)
+	this.Unlock()
+
+	return this
+}
+
+func (this *Scheduler) runAll(locNow time.Time) {
+
+	for _, taskItem := range this.tasks {
+		locTask := taskItem
+
+		go func(task *Task) {
+			_, _ = task.run(locNow)
+		}(locTask)
+	}
+
+	return
+}
+
+func Start(context context.Context) {
+	if schedule == nil {
+		newScheduler()
+	}
+
+	if schedule.running {
+		return
+	}
+
+	schedule.Lock()
+	schedule.running = true
+	schedule.time = time.NewTicker(1 * time.Second)
+	schedule.Unlock()
+
+	go func() {
+		for {
+			select {
+			case locNow := <-schedule.time.C:
+				schedule.runAll(locNow)
+
+			case <-context.Done():
+				schedule.time.Stop()
+				schedule.checkTaskStatus(true)
+				return
+			}
+		}
+	}()
+}
---
+++ schedule_test.go
@@ -0,0 +1,97 @@
+/*
+ *
+ *
+ *  * Copyright 2019 koalaone@163.com
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *       http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package schedule
+
+import (
+	"context"
+	"log"
+	"testing"
+	"time"
+)
+
+func EventSecond(param string) {
+	log.Printf("second event value:%v\n", param)
+}
+
+func EventSecondWithWait(param string) {
+	log.Printf("second event value:%v\n", param)
+
+	time.Sleep(12 * time.Second)
+}
+
+func EventMinute(param string) {
+	log.Printf("minute event value:%v\n", param)
+}
+
+func EventHour(param string) {
+	log.Printf("hour event value:%v\n", param)
+}
+
+func EventDay(param string) {
+	log.Printf("day event value:%v\n", param)
+}
+
+func EventAtDatetime(param string) {
+	log.Printf("AtDatetime event value:%v\n", param)
+}
+
+func TestScheduler(t *testing.T) {
+	err := EverySeconds(1).Do(EventSecond, "second")
+	if err != nil {
+		t.Errorf("test schedule EventSecond error:%v", err.Error())
+		return
+	}
+
+	err = EverySeconds(2).Do(EventSecondWithWait, "second")
+	if err != nil {
+		t.Errorf("test schedule EventSecondWithWait error:%v", err.Error())
+		return
+	}
+
+	err = EveryMinutes(1).Do(EventMinute, "minute")
+	if err != nil {
+		t.Errorf("test schedule EventMinute error:%v", err.Error())
+		return
+	}
+
+	err = EveryHours(1).Do(EventHour, "hour")
+	if err != nil {
+		t.Errorf("test schedule EventHour error:%v", err.Error())
+		return
+	}
+
+	err = EveryDays(1).Do(EventDay, "day")
+	if err != nil {
+		t.Errorf("test schedule EventDay error:%v", err.Error())
+		return
+	}
+
+	err = AtDateTime(2018, time.December, 21, 16, 59, 10).
+		Do(EventAtDatetime, "at_datetime")
+	if err != nil {
+		t.Errorf("test schedule AtDateTime error:%v", err.Error())
+		return
+	}
+
+	ctx, _ := context.WithCancel(context.Background())
+	Start(ctx)
+
+	select {}
+}
